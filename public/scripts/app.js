e126e4119373503cf73f42c2ee843115e1b087f7
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-07-20 13:47:58 +0200

    Added Typewriting effect to index page (Thanks to mario!)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,6 +10,7 @@ class PageHandler {
         this.setThemeIcon();
       })
     );
+    this.typeWriterManager();
   }
 
   setThemeIcon() {
@@ -19,6 +20,46 @@ class PageHandler {
         <img src="/icons/${isDark ? "sun" : "moon"}.svg" alt="${isDark ? "sun" : "moon"}" class="w-6 h-6" />
       `;
     });
+  }
+
+  typeWriterManager() {
+    let dynamicHeaderElement = document.querySelector("#typewriter")
+    if (dynamicHeaderElement == null) return;
+
+    let dynamicHeaderText = ["Finn", "a Physicist", "Finn", "a Space Nerd", "Finn", "a Science Fan", "Finn", "a Coding Guy"];
+    
+    let i = 0;
+    let currentText = dynamicHeaderText[i];
+    let currentWord = dynamicHeaderText[i];
+    let isDeleting = false;
+
+    let typeWriter = () => {
+      currentWord = dynamicHeaderText[i];
+      if(isDeleting){
+        currentText = currentWord.substring(0, currentText.length - 1);
+      }
+
+      if(!isDeleting){
+        currentText = currentWord.substring(0, currentText.length + 1);
+      }
+      dynamicHeaderElement.innerHTML = currentText;
+      if(!isDeleting && currentText === currentWord){
+        isDeleting = true;
+        setTimeout(typeWriter, 4000);
+
+      } else if (isDeleting && currentText === ""){
+        isDeleting = false;
+        i++;
+
+        if(i === dynamicHeaderText.length){
+          i = 0;
+        }
+        setTimeout(typeWriter, 500);
+      } else {
+        setTimeout(typeWriter, 100)
+      }
+    };
+    typeWriter();
   }
 }
 
