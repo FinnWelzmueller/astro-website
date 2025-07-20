31f68733e7653f7672c40f530933b15d9e0f3bff
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-07-20 14:57:56 +0200

    added lightcolors for cakeChart to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,15 @@ module.exports = {
       colors: {
         light: "#faf0e6",
         dark: "#111827",
-        accentLight: "#f97316",
+        accentLight: "#679436",
         accentDark: "#1e3a8a",
-        tagLight: "#f3f4f6",
+        tagLight: "#679436",
         tagDark: "#6b7280",
-        buttonLight: "#f97316",
-        buttonDark: "#1e3a8a"
+        buttonLight: "#A5BE00",
+        buttonDark: "#1e3a8a",
+        cakeChartBackLight: "#FFE2D1",
+        cakeChartAccentLight: "#A5BE00",
+        cakeChartFieldsLight: "#468C98"
       }
     },
   },
