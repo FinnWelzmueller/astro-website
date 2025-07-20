ccb3993a5be46662dd2695fa65709ce9df2fcd02
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-07-20 11:45:23 +0200

    implemented alternative darkmode switch that works for mobile and desktop views

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,25 @@
 class PageHandler {
-    constructor() {
-      this.setThemeIcon();
-      document.querySelector("#colorSwitch")?.addEventListener("click", () => {
+  constructor() {
+    this.colorSwitchButtons = document.querySelectorAll(".colorSwitch");
+    this.setThemeIcon();
+
+    this.colorSwitchButtons.forEach((btn) =>
+      btn.addEventListener("click", () => {
         const isDark = document.documentElement.classList.toggle("dark");
         localStorage.setItem("theme", isDark ? "dark" : "light");
         this.setThemeIcon();
-      });
-    }
-  
-    setThemeIcon() {
-      const colorSwitch = document.querySelector("#colorSwitch");
-      if (!colorSwitch) return;
-      const isDark = document.documentElement.classList.contains("dark");
-      colorSwitch.innerHTML = `
+      })
+    );
+  }
+
+  setThemeIcon() {
+    const isDark = document.documentElement.classList.contains("dark");
+    this.colorSwitchButtons.forEach((btn) => {
+      btn.innerHTML = `
         <img src="/icons/${isDark ? "sun" : "moon"}.svg" alt="${isDark ? "sun" : "moon"}" class="w-6 h-6" />
       `;
-    }
+    });
   }
-  
-  document.addEventListener("DOMContentLoaded", () => new PageHandler());
-  
+}
+
+document.addEventListener("DOMContentLoaded", () => new PageHandler());
