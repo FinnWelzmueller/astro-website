036b2341e384f45929286622cf60a08c2a2b0220
Finn Welzmüller <finnwelzmueller97@gmail.com>
2025-08-15 20:53:08 +0200

    changed colours in light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        light: "#faf0e6",
+        light: "#FAF7F2",
         dark: "#111827",
         accentLight: "#679436",
         accentDark: "#1e3a8a",
@@ -15,9 +15,9 @@ module.exports = {
         tagDark: "#6b7280",
         buttonLight: "#A5BE00",
         buttonDark: "#1e3a8a",
-        cakeChartBackLight: "#FFE2D1",
-        cakeChartAccentLight: "#A5BE00",
-        cakeChartFieldsLight: "#468C98"
+        cakeChartBackLight: "#FFF9F5",
+        cakeChartAccentLight: "#F5EDE6",
+        cakeChartFieldsLight: "#F9F6F2"
       }
     },
   },
